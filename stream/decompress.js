cabcb1e659b4b2a25020b8a86abfac9ba1a479d7
lxtXintao <lxt_ysu@163.com>
2017-07-07 15:39:59 +0800

    Create decompress.js

---
+++ stream/decompress.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+//解压　input.txt.gz 文件为input.txt
+fs.createReadStream('input.txt.gz').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('input1.txt'));
+
+console.log("文件解压完成．");
+
